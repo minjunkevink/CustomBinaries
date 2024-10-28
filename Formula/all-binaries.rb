b07664eddd8f874b0ee0eb3eda375f26b050d155
minjunkevink <kimkj@usc.edu>
2024-10-28 10:41:01 -0700

    gitp update

--- Formula/all-binaries.rb
+++ Formula/all-binaries.rb
@@ -1,12 +1,12 @@
 class AllBinaries < Formula
     desc "Installs all custom binaries"
-    homepage "https://github.com//custombinaries"
-    url "https://github.com/minjunkevink/custombinaries/archive/refs/heads/main.zip"
+    homepage "https://github.com//CustomBinaries"
+    url "https://github.com/minjunkevink/CustomBinaries/archive/refs/heads/main.zip"
     version "1.0.0"
     sha256 "<SHA256 hash of the ZIP>"
   
     # List each binary as a dependency
-    depends_on "minjunkevink/custombinaries/gitp"
+    depends_on "minjunkevink/CustomBinaries/gitp"
   
     def install
         #TODO: add file configs
