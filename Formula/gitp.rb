0d48084e164f74cc0d70833b3e5bb441a452ca1a
minjunkevink <kimkj@usc.edu>
2024-10-28 10:43:57 -0700

    Update gitp formula with new SHA256 hash

--- Formula/gitp.rb
+++ Formula/gitp.rb
@@ -3,7 +3,7 @@ class Gitp < Formula
     homepage "https://github.com/minjunkevink/CustomBinaries"
     url "https://github.com/minjunkevink/CustomBinaries/archive/refs/heads/main.zip"
     version "1.0.0"
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    sha256 "0534bf9fdf75356d66dcf0d74a9b16d802b30bb7051bd8d87745c3339eae286b"
   
     def install
       bin.install "bin/gitp"
