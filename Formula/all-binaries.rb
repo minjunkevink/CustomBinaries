e0c0e2f1d138b009993f599b416fc1e9cc12a4cb
minjunkevink <kimkj@usc.edu>
2024-10-28 11:06:23 -0700

    all-binaries udpate, SHA key managment

--- Formula/all-binaries.rb
+++ Formula/all-binaries.rb
@@ -3,12 +3,13 @@ class AllBinaries < Formula
     homepage "https://github.com//CustomBinaries"
     url "https://github.com/minjunkevink/CustomBinaries/archive/refs/heads/main.zip"
     version "1.0.0"
-    sha256 "<SHA256 hash of the ZIP>"
+    sha256 "4a3581ddb149dc392eb743f82764b98ccacd195448402033c5db0087c6d84599"
   
     # List each binary as a dependency
     depends_on "minjunkevink/CustomBinaries/gitp"
   
     def install
-        #TODO: add file configs
+        bin.install "bin/gitp"
+        bin.install "bin/clean_temp" 
     end
   end
--- Formula/clean_temp.rb
+++ Formula/clean_temp.rb
@@ -1,10 +1,10 @@
-class Gitp < Formula
-    desc "A command that runs 'git add . && git commit -m <message> && git push' with an optional message parameter"
+class CleanTemp < Formula
+    desc "Clean temporary files in the home directory"
     homepage "https://github.com/minjunkevink/CustomBinaries"
     url "https://github.com/minjunkevink/CustomBinaries/archive/refs/heads/main.zip"
     version "1.0.0"
   
     def install
-      bin.install "bin/gitp"
+      bin.install "bin/clean_temp"
     end
   end
